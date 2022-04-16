7655a8cf1e3fdc090b8e68b36b3449718f7ef11e
Olumide Oyegoke <59411943+olumide1128@users.noreply.github.com>
2022-04-16 19:21:28 +0100

    Created a LightServer file to start the Light Server

---
+++ src/main/java/smartsec/lightService/LightServer.java
@@ -0,0 +1,43 @@
+package smartsec.lightService;
+
+import java.io.IOException;
+
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+import simpleJMDNS.SimpleServiceRegistration;
+
+public class LightServer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int port = 50052;
+		String service_type = "light_grpc._tcp.local.";
+		String service_name = "lightGrpcServer";
+		SimpleServiceRegistration ssr = new SimpleServiceRegistration();
+		ssr.run(port, service_type, service_name);
+		
+	    
+		try {
+			Server server = ServerBuilder.forPort(port)
+			    .addService(new lightServiceImpl())
+			    .build()
+			    .start();
+			System.out.println("\nLight Server Started");
+			
+			 server.awaitTermination();
+
+			 
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	    
+	    System.out.println("Light Server started, listening on " + port);
+	}
+
+}
